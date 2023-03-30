a34997dba1be38d806a2204b2d8049f14d76c40a
pythonnelson <isaacnsbkargbo20@gmail.com>
2023-03-30 19:07:40 +0000

    Add environment variables

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dg=ijl9m9+gr=5=uta_6d@zinaj$en2!ze-o3md6%t_o203ht6'
+# SECRET_KEY = 'django-insecure-dg=ijl9m9+gr=5=uta_6d@zinaj$en2!ze-o3md6%t_o203ht6'
 
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = []
+DEBUG = config('DEBUG', cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv()) 
 
 
 # Application definition
@@ -152,5 +156,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 load_dotenv()
 
 #JWT SECRET KEY
-# JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
-JWT_SECRET_KEY='JAKSHJFKAHSDICIENCKEJNCJKAHHKJAHFIUEFSJDFAHSDFAJHFAJK'
+JWT_SECRET_KEY = config('JWT_SECRET_KEY')
+# JWT_SECRET_KEY='JAKSHJFKAHSDICIENCKEJNCJKAHHKJAHFIUEFSJDFAHSDFAJHFAJK'
