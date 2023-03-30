f29c02b5268158065675f419afba75f71f9c1d75
pythonnelson <isaacnsbkargbo20@gmail.com>
2023-03-30 15:29:26 +0000

    Updated .env and settings file

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,6 +135,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+load_dotenv()
 
 #JWT SECRET KEY
 JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
