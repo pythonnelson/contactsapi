a9807fb99f2e51b735c50614eddb0831b28197a6
pythonnelson <isaacnsbkargbo20@gmail.com>
2023-03-30 19:58:49 +0000

    Update staticfiles

--- contactsapi/urls.py
+++ contactsapi/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -27,4 +29,4 @@ urlpatterns = [
     # API Documentation urls
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("redoc", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
