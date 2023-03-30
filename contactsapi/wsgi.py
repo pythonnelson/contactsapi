7343f005b56d8bfc3d74a9d51423d562e49ac4c2
pythonnelson <isaacnsbkargbo20@gmail.com>
2023-03-30 19:00:47 +0000

    Ready for Vercel Deployment

--- contactsapi/wsgi.py
+++ contactsapi/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contactsapi.settings')
 
 application = get_wsgi_application()
+
+# Import for vercel deployment
+app = application
